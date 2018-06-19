068cbb8fe7a4514e997c6f3404854ff0717b03d1
Monica Tamaru Jones <mntamaru@gmail.com>
2018-06-19 16:35:32 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
   
-  namespace: :admin do
+  namespace :admin do
     resources :stats, only: [:index]
   end
   
